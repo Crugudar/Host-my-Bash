4226e0c334015a8273af99b423f658b87b12feba
Carla Navia <carlanavia@MBP-de-Carla.home>
2020-10-28 19:57:06 +0100

    views

---
+++ models/Reviews 2.js
@@ -0,0 +1,13 @@
+// *Reviews{
+//     user: {ObjectId},
+//     reviewTitle: String,
+//     comment: String,
+//     commentTo: {???????????????}
+//   } ,
+//   {
+//     timestamps: {
+//       createdAt,
+//       updatedAt
+//     }
+//   }
+  
