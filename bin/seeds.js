39ca8483b3cbce7075ccce443892402abfa5d6bf
Carla Navia <carlanavia@MBP-de-Carla.home>
2020-10-28 17:56:52 +0100

    base2

---
+++ bin/seeds.js
@@ -0,0 +1,13 @@
+// Estancia {
+//     -imagen:{type:String}
+//     -Nombre estancia{type:string, required:true}
+//     -Owner:{user(hotel)_id},
+//     -streetName: String,
+//     -streetNumber: Number,
+//     -city: String,
+//     -zipcode:{Number}
+//     -phone: Number,
+//     -webpage: String,
+//     -paquete [{String}, {String}],
+//     -fechas disponibilidad:[{type:Date}] (modificar con update en la reserva)
+//     }
