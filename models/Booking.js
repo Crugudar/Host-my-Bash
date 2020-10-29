1166cab1b2b1192d22aaa1fc5001908a7e2a6c64
Carla Navia <carlanavia@MBP-de-Carla.home>
2020-10-29 14:44:27 +0100

    main

--- models/Booking.js
+++
@@ -1,9 +0,0 @@
-// Booking{
-//     nameUser:{user_id}
-//     Owner:{hotel_id}
-//     Place:{estancia_id}
-//     fecha{Date}
-//     paquete enum [{Place.paquete}]
-//     invitados
-//     }
-    
