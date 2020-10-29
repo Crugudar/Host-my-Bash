39cad1659a9683eaae3373cc47eacd54ed846ffd
Carla Navia <carlanavia@MBP-de-Carla.home>
2020-10-29 14:42:03 +0100

    user

--- models/User.js
+++ models/User.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
    
     username: { type:String, required:true, unique:true},
-    email:{type String, required:true, unique:true},
+    email:{type: String, required:true, unique:true},
     password:{type:String, required:true},
-    isHotel:false;
+    isHotel:false,
     reservations:[{type:Sring}],
     estancias:[{type:String}],
   });
