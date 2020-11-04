b781f3f3a7537f47c7cab51702f906b1f27181af
Carla Navia <carlanavia@MBP-de-Carla.home>
2020-11-04 14:09:03 +0100

    add a plan - hotel a medias

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
+const uploadCloud = require("../config/cloudinary");
 // User model
 const User = require("../models/User");
 
@@ -10,87 +10,139 @@ const bcrypt = require("bcryptjs");
 //requerimos paquete 'jsonwebtoken'
 const jwt = require("jsonwebtoken");
 
-// //agregar este middleware 
-const withAuth = require('../helpers/middleware');
+// //agregar este middleware
+const withAuth = require("../helpers/middleware");
+const Plan = require("../models/Plan");
 
 //Ruta GET - Signup
 router.get("/hotelsignup", function (req, res, next) {
-    res.render("auth/hotelsignup", { errorMessage: "" });
-  });
-  
-  //Ruta POST - Signup
-  router.post("/hotelsignup", async (req, res, next) => {
-    const {
-      username,
-      lastname,
-      email,
-      verifyemail,
-      password,
-      verifypassword,
-    } = req.body;
-    //Chequear si hay algún campo vacío en el formulario
-    if (
-      username == "" ||
-      email == "" ||
-      verifyemail == "" ||
-      password == "" ||
-      verifypassword == ""
-    ) {
+  res.render("auth/hotelsignup", { errorMessage: "" });
+});
+
+//Ruta POST - Signup
+router.post("/hotelsignup", async (req, res, next) => {
+  const {
+    username,
+    lastname,
+    email,
+    verifyemail,
+    password,
+    verifypassword,
+  } = req.body;
+  //Chequear si hay algún campo vacío en el formulario
+  if (
+    username == "" ||
+    email == "" ||
+    verifyemail == "" ||
+    password == "" ||
+    verifypassword == ""
+  ) {
+    res.render("auth/hotelsignup", {
+      errorMessage: "Some fields are incomplete.",
+    });
+    return;
+  }
+  //Buscar si el usuario ya está en nuestra BBDD
+  try {
+    const user = await User.findOne({ email: email });
+    if (user !== null) {
       res.render("auth/hotelsignup", {
-        errorMessage: "Some fields are incomplete.",
+        errorMessage: "The email already exists!",
       });
       return;
     }
-    //Buscar si el usuario ya está en nuestra BBDD
-    try {
-      const user = await User.findOne({ email: email });
-      if (user !== null) {
-        res.render("auth/hotelsignup", {
-          errorMessage: "The email already exists!",
-        });
-        return;
-      }
-      //Crear usuario + generar password
-      const salt = bcrypt.genSaltSync(10);
-      const hashPass = bcrypt.hashSync(password, salt);
-  
-      await User.create({
-        username,
-        email,
-        password: hashPass,
-        isHotel:true
-      });
-      res.redirect("/login");
-    } catch (error) {
-      next(error);
-    }
-  });
-  
-  router.get("/hotelprofile", withAuth, async (req, res, next) => {
-    try {
-      const thisUser = await User.findOne({ _id: req.userID }).populate({
+    //Crear usuario + generar password
+    const salt = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    await User.create({
+      username,
+      email,
+      password: hashPass,
+      isHotel: true,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/hotelprofile", withAuth, async (req, res, next) => {
+  try {
+    const thisUser = await User.findOne({ _id: req.userID }).populate({
       path: "plans",
       model: "Plan",
       // populate: { path: "plan", model: "Plan" },
     });
-  
-    if(res.locals.currentUserInfo) {
-      let tieneHotel = false
-      const user = res.locals.currentUserInfo
+
+    if (res.locals.currentUserInfo) {
+      let tieneHotel = false;
+      let tieneReservas = false;
+      const user = res.locals.currentUserInfo;
       //console.log ("HOLAAAAAAAAA", user.isHotel)
+
+      user.isHotel ? (tieneHotel = true) : (tieneHotel = false);
+      for(let i=0; i<thisUser.plans.length; i++) {
+        thisUser.plans[i].reserved.length>0 ? (tieneReservas = true) : (tieneReservas = false);
+      }
+      //user.plans.length > 0 ? (tieneReservas = true) : (tieneReservas = false);
+
+      console.log("USERS PLANS LENGTH", user.plans.length)
       
-      user.isHotel ? tieneHotel = true : tieneHotel = false
-      //console.log("FUNCIONAAAAAA?????", tieneHotel)
-      res.render("users/hotel_profile", {tieneHotel:tieneHotel,thisUser:thisUser});
-      
+      res.render("users/hotel_profile", {
+        tieneHotel: tieneHotel,
+        tieneReservas: tieneReservas,
+        thisUser: thisUser,
+      });
     } else {
       res.render("users/hotel_profile");
     }
-    
+  } catch (err) {
+    console.log(err);
   }
-catch (err){
-  console.log (err)
-}});
+});
 
+//Ruta crear plan GET
+router.get("/createaplan", withAuth, (req, res, next) => {
+  res.render("users/addPlan");
+});
+
+//Ruta crear plan POST
+router.post("/createaplan", uploadCloud.single('image'), withAuth, async (req, res, next) => {
+  const image  = req.file.url;
+  const {
+    planName,
+    description,
+    streetName,
+    streetNumber,
+    zipcode,
+    city,
+    phone,
+  } = req.body;
+  
+
+  const createPlan = {
+    image,
+    planName,
+    description,
+    streetName,
+    streetNumber,
+    zipcode,
+    city,
+    phone, }
+
+  const newPlan = await Plan.create(createPlan);
+  
+  await User.findByIdAndUpdate(
+    req.userID, {
+      $push: {
+        plans: newPlan._id
+      }
+    }, {
+      new: true
+    }
+  );
+    res.redirect("/hotelprofile")
+});
 
 module.exports = router;
